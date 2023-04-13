a91fb189f8193f4573476971fbb69ddb203e6cd0
tejasviii <102219382+tejasviii@users.noreply.github.com>
2023-04-13 20:48:19 +0530

    Create zippopotam.java

---
+++ zippopotam/zippopotam.java
@@ -0,0 +1,21 @@
+package org.example;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class zippopotam {
+
+    public static void main(String[] args) throws Exception {
+        String url = "https://api.zippopotam.us/us/33162";
+
+        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
+
+        HttpClient httpClient = HttpClient.newBuilder().build();
+
+        HttpResponse<String> httpResponse = httpClient.send(request,HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(httpResponse.statusCode());
+        System.out.println(httpResponse.body());
+    }
+}
